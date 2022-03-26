bfc3ae5cc0f22312b10ede32104f7cb4a58a991c
Gabriel <gonzagagabriel459@gmail.com>
2022-03-25 22:10:41 -0300

    MODIFICAÇÕES

---
+++ script2_01.js
@@ -0,0 +1,10 @@
+//document.write("Hello World!!<hr>");
+document.write("Alo mundo!!");//estará comentado(anotado)
+/*
+Posso escrever as minhas
+anotações do código
+para colocar uma tag HTML
+deve estar entre aspas duplas
+*/
+// em uma linha
+/* escrevo em uma linha */
